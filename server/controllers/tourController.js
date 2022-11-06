84d42466c8a002d69eff37187eea6ca46d303130
AyushShende <ayushshende9@gmail.com>
2022-11-06 20:18:12 +0530

    built tour resource: model, routes, controllers

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,11 +1,72 @@
 import Tour from "../models/Tour.js";
+import { catchAsync } from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
 
-export const getAlltours = () => {};
+export const getAlltours = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
 
-export const getTour = () => {};
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
-export const createTour = () => {};
+export const getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
 
-export const updateTour = () => {};
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
 
-export const deleteTour = () => {};
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+export const createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: newTour,
+    },
+  });
+});
+
+export const updateTour = catchAsync(async (req, res, next) => {
+  const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!updatedTour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: updatedTour,
+    },
+  });
+});
+
+export const deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
--- server/models/Tour.js
+++ server/models/Tour.js
@@ -1,5 +1,71 @@
 import mongoose from "mongoose";
 
-const tourSchema = new mongoose.Schema({});
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+      maxLength: [40, "A tour cannot have more than 40 characters"],
+      minLength: [10, "A tour must have more than 10 characters"],
+    },
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium or difficult",
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1"],
+      max: [5, "Rating must be below 5"],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) must be below regular price",
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"],
+    },
+    images: [String],
+    startDates: [Date],
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Tour", tourSchema);
